fb25c8f13873ef3fcfd8991cd76d219ff50c221a
Ryan Olson <rolson@nvidia.com>
2026-02-04 06:02:26 +0000

    fix: remove loom-macros/target from tracking and fix gitignore
    The gitignore had /target/ (root only) instead of target/ (all dirs).
    This allowed loom-macros/target/ build artifacts to be committed.
    
    Signed-off-by: Ryan Olson <rolson@nvidia.com>

--- loom-macros/target/debug/build/quote-6e9809c909a5e12f/build_script_build-6e9809c909a5e12f.d
+++
@@ -1,5 +0,0 @@
-/home/ryan/repos/loom/loom-macros/target/debug/build/quote-6e9809c909a5e12f/build_script_build-6e9809c909a5e12f.d: /home/ryan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.44/build.rs
-
-/home/ryan/repos/loom/loom-macros/target/debug/build/quote-6e9809c909a5e12f/build_script_build-6e9809c909a5e12f: /home/ryan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.44/build.rs
-
-/home/ryan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.44/build.rs:
